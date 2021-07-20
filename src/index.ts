42cbba0836d1e13982a877146a2fc64bf0b65d9c
TheBusyBiscuit <mrCookieSlime@gmail.com>
2021-07-20 10:59:30 +0200

    Load config from default location

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as discord from './discord'
-import { Config } from './types';
+import { Config } from './types'
+import * as cfg from './config'
 
-function run(cfg: Config) {
+async function run(): Promise<any> {
     let start = Date.now()
 
-    discord.sendEmbed(cfg).then(() => {
+    return discord.sendEmbed(cfg).then(() => {
         console.log(`Webhook delivered successfully in ${Number((Date.now() - start) / 1000).toFixed(2)} second(s)!`)
         process.exit(0)
     }).catch(err => {
